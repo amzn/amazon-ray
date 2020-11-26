e79f7595219efaed57ea274d88585fb15bdd4ace
Patrick Ames <pdames@amazon.com>
2020-11-26 00:26:34 -0800

    Set npm config python path to python 3.6 when building the dashboard into linux wheels.

--- python/build-wheel-manylinux2014.sh
+++ python/build-wheel-manylinux2014.sh
@@ -56,6 +56,9 @@ nvm use "$NODE_VERSION"
 # Build the dashboard so its static assets can be included in the wheel.
 # TODO(mfitton): switch this back when deleting old dashboard code.
 pushd python/ray/new_dashboard/client
+  # Python >= 3.5.0 is required to successfully rebuild node-gyp.
+  echo "Setting npm config python path to /opt/python/${PYTHONS[0]}/bin/python"
+  npm config set python "/opt/python/${PYTHONS[0]}/bin/python"
   npm ci
   npm run build
 popd
